fe47f9c98f5e58ad709ba8525feb5d7d4ce1f7dc
charlesantunes <charleswcantunes@gmail.com>
2023-05-19 20:03:11 -0300

    add modal

--- src/service/script.js
+++ src/service/script.js
@@ -1,3 +1,4 @@
+//carrossel do index
 const imgContainer = document.querySelector("#img-carrossel");
 const img = document.querySelectorAll("#img-carrossel img");
 
@@ -14,3 +15,23 @@ function carrossel() {
 }
 
 setInterval(carrossel, 2000);
+
+//=================================================================
+
+//Modal do botão contato
+const openModalBtn = document.querySelector("#open-modal");
+const closeModalBtn = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () =>{ /*toogle() é uma func que se estiver habilitado um item ele desabilita ou contrário.*/
+    /*lógica foi melhorada abaixo 
+    modal.classList.toggle("hide");
+    fade.classList.toggle("hide");
+    */
+    [modal,fade].forEach((e)=>e.classList.toggle("hide"))
+}
+
+[openModalBtn,closeModalBtn,fade].forEach((e)=>{
+    e.addEventListener("click",()=> {toggleModal()})
+})
